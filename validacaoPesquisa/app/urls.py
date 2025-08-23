d3665a381aa21d9f61bce23fc9c3d1d69a910969
ianmelo1 <ianmelo171@gmail.com>
2025-08-22 21:00:34 -0300

    Função delete e alguns ajustes na rota "home"

--- validacaoPesquisa/app/urls.py
+++ validacaoPesquisa/app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
+    path('', listar_carro, name='home'),
     path('cadastrar_carro/', cadastrar_carro, name='cadastrar_carro'),
     path('listar_carro/', listar_carro, name='carro_list'),
     path('editar_carro/<int:pk>/', editar_carro, name='editar_carro'),
